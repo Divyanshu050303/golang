07a12fbea6b0255413cc589e3433bf814832ab1f
Divyanshu050303 <divyanshusingh7060@gmail.com>
2024-05-18 13:06:41 +0530

    first commit

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"golang-syntax/syntax"
+)
+
+func main() {
+fmt.Println("Hello world!");
+syntax.DatType();
+}
---
+++ syntax/data-type.go
@@ -0,0 +1,16 @@
+package syntax
+
+import (
+"fmt"
+)
+
+func DatType(){
+//  there are so may data types in golang
+// bool
+// String
+// int int4 int 8 int16 int32 int64
+// and many more
+
+
+fmt.Println("Data Type in golang");
+}
