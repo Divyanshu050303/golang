821f3e1ee95fc028494f37319e8de4f728e0390d
Divyanshu050303 <divyanshusingh7060@gmail.com>
2024-05-23 18:19:49 +0530

    http request in golang

--- main.go
+++ main.go
@@ -2,20 +2,47 @@ package main
 
 import (
 	"fmt"
-	"golang-syntax/syntax"
+	"net/http"
+
+	"golang-syntax/curd_operation/controller"
+
+	"github.com/julienschmidt/httprouter"
+	"gopkg.in/mgo.v2"
 )
 
 func main() {
-fmt.Println("Hello world!");
-// syntax.DatType();
-// syntax.Variables();
-//  syntax.Loops()
-// syntax.StatementSs();
-// syntax.ArrayOperation();
 
-// syntax.Input()
+	// syntax.DatType();
+	// syntax.Variables();
+	//  syntax.Loops()
+	// syntax.StatementSs();
+	// syntax.ArrayOperation();
 
-// syntax.Gomap();
+	// syntax.Input()
 
-syntax.GoSlice()
+	// syntax.Gomap();
+
+	// syntax.GoSlice()
+	fmt.Println("Hello world!")
+
+	r := httprouter.New()
+	uc := controller.NewUserController(getSession())
+
+	r.GET("/user/:id", uc.GetUser)
+	r.POST("/user", uc.CreateUser)
+	r.DELETE("/user/:id", uc.DeleteUser)
+
+	err := http.ListenAndServe("localhost:8080", r)
+	if err != nil {
+		fmt.Println("Failed to start server:", err)
+	}
+}
+
+func getSession() *mgo.Session {
+	s, err := mgo.Dial("mongodb://localhost:27017")
+	if err != nil {
+		fmt.Println("Failed to connect to MongoDB:", err)
+		panic(err)
+	}
+	return s
 }
